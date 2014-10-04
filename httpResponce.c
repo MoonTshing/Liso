71aeaa0cbe79bce4309fd3291e1945dcd70effc9
chanjuaz <chanjuaz@andrew.cmu.edu>
2014-10-03 23:41:43 -0400

    “autolab”

--- httpResponce.c
+++ httpResponce.c
@@ -167,17 +167,12 @@ int statusAdd(requestLine *req)
 
 
 int contentType(requestLine *req){
-	char *extension=NULL;
-	
-	if(req->status == 200)
+	char *extension;
+	if(!(extension = strrchr(req->relativePath, '.')))
 	{
-		if(!(extension = strrchr(req->relativePath, '.')))
-		{
-			req->status = 404;
-			return 0;
-		}
+		req->status = 404;
+		return 0;
 	}
-		
 	if(!strcmp(extension,".jpg")){
 		strcpy(req->contentType,"image/jpeg");
 	}else if(!strcmp(extension,".png")){
